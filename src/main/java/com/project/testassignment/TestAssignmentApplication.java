9147eca25f7866a85269d705f960d318698e0de5
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-03-17 16:58:39 +0300

    Initial Commit

---
+++ src/main/java/com/project/testassignment/TestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.testassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/testassignment/TestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.testassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
